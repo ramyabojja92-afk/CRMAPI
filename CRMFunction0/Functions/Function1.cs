85935fab9b774bfaccff5faf032c3905412e5873
ramyabojja92-afk <ramyabojja92@gmail.com>
2026-05-27 16:14:07 +0200

    changed code

--- CRMFunction0/Functions/Function1.cs
+++ CRMFunction0/Functions/Function1.cs
@@ -1,37 +1,45 @@
-using CRMFunction0.Entities;
+
 using CRMFunction0.Entities;
 using CRMFunction0.Services;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace CRMFunction0.Functions;
-
-public class CosmosDBTrigger
+namespace CRMFunction0.Functions
 {
-    private readonly EmailService _emailService;
-    private readonly ILogger<CosmosDBTrigger> _logger;
-
-    public CosmosDBTrigger(
-        EmailService emailService,
-        ILogger<CosmosDBTrigger> logger)
+    public class CosmosDBTrigger
     {
-        _emailService = emailService;
-        _logger = logger;
-    }
+        private readonly ILogger<CosmosDBTrigger> _logger;
+        private readonly EmailService _emailService;
 
-    [Function("CosmosDBTrigger")]
-    public void Run(
-    [CosmosDBTrigger(
-        databaseName: "CRMDB",
-        containerName: "Customers",
-        Connection = "CosmosDb",
-        LeaseContainerName = "leases",
-        CreateLeaseContainerIfNotExists = true)]
-    IReadOnlyList<Customer> input)
-    {
-        _logger.LogInformation("TRIGGER FIRED!");
+        public CosmosDBTrigger(
+            ILogger<CosmosDBTrigger> logger,
+            EmailService emailService)
+        {
+            _logger = logger;
+            _emailService = emailService;
+        }
+
+        [Function("CosmosDBTrigger")]
+        public async Task Run(
+            [CosmosDBTrigger(
+                databaseName: "CRMDB",
+                containerName: "Customers",
+                Connection = "CosmosDb",
+                LeaseContainerName = "leases",
+                CreateLeaseContainerIfNotExists = true)]
+            IReadOnlyList<Customer>? customers)
+        {
+            if (customers is null || customers.Count == 0)
+            {
+                _logger.LogInformation("No customer changes detected.");
+                return;
+            }
+
+            foreach (var customer in customers)
+            {
+                
+                await _emailService.SendEmailAsync(customer);
+            }
+        }
     }
 }
-
-
-    
